53a83c621ec0e8c1c2ed3bbc86f592567f1dec87
sohan-lv <sohanlv1@gmai.com>
2025-03-16 19:52:43 -0400

    EDA and Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -15,3 +16,10 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+# Example usage:
+if __name__ == "__main__":
+    try:
+        1 / 0
+    except Exception as e:
+        logging.info("An exception occurred")
+        raise CustomException(e, sys) from e
